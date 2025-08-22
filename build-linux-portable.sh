e1b3787a110c0ca3542fd8f60e9bb0149d050eff
tbxark <tbxark@outlook.com>
2025-08-22 13:17:23 +0800

    refactor: 更新build-linux-portable.sh以使用dockcross/linux-x86替代rockylinux:9.6

--- build-linux-portable.sh
+++ build-linux-portable.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-docker pull rockylinux/rockylinux:9.6
-docker run --rm -v "$(pwd)":/work rockylinux/rockylinux:9.6 /bin/bash -c './build.sh'
+docker pull dockcross/linux-x86
+docker run --rm -v "$(pwd)":/work -w /work dockcross/linux-x86 /bin/bash -c './build.sh'
 echo "==> Done. Artifacts are in build/install"
